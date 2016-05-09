bd5a6e7b966db086df5c427f223ef01c4def4da8
lmavila <lmavila@gmail.com>
2016-05-08 19:47:27 -0700

    bug fixes

--- profiling/exploratory.R
+++ profiling/exploratory.R
@@ -1,3 +1,4 @@
+#explores the parents file
 date()
 parents<-read.table('/group/jrigrp4/phasing/cj_teo_updated/teo_parents_hap_AGPv2.txt',header=TRUE);
 date()
@@ -6,3 +7,9 @@ summary(parents[,1:7])
 date()
 levels(as.factor(parents$chr))
 levels(as.factor(parents[,4]))
+
+chromosomes<-as.character(levels(as.factor(parents$chr)))
+
+summary(parents[parents$chr==chromosomes[1],1:5])
+
+
